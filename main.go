f8e47550c5fb63ec7ed47ac1a1e80edfc4609717
Akene-Uzezi <xtremejoexj@gmail.com>
2026-08-05 17:10:18 +0100

    update the problem with the ascii that it shows

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "go-scaffold",
+	Short: "A CLI tool meant to scaffold new Go projects",
+	Long: `
+  ________                   _________              _____  _____      .__       .___
+ /  _____/  ____            /   _____/ ____ _____ _/ ____\/ ____\____ |  |    __| _/
+/   \  ___ /  _ \   ______  \_____  \_/ ___\\__  \\   __\\   __\/  _ \|  |   / __ |
+\    \_\  (  <_> ) /_____/  /        \  \___ / __ \|  |   |  | (  <_> )  |__/ /_/ |
+ \______  /\____/          /_______  /\___  >____  /__|   |__|  \____/|____/\____ |
+        \/                         \/     \/     \/                              \/
+
+This is go scaffold. A CLI tool meant to scaffold new Go projects.`,
+}
+
 func main() {
-	fmt.Println("main")
+	if err := rootCmd.Execute(); err != nil {
+		log.Printf("An Error occured: %s", err)
+		os.Exit(1)
+	}
 }
